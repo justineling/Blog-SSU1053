299c071f7e2dd1876558f48ca3b745cb39d1955e
justineling <justinelingkeyi@gmail.com>
2025-01-08 18:20:04 +0800

    反转排序

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -16,16 +16,16 @@ export default sidebar({
       icon: "book",
       prefix: "posts/reflections",
       children: [
-        "/posts/reflections/1",
-        "/posts/reflections/2",
-        "/posts/reflections/3",
-        "/posts/reflections/4",
-        "/posts/reflections/5",
-        "/posts/reflections/6",
-        "/posts/reflections/7",
-        "/posts/reflections/8",
+        "/posts/reflections/10",
         "/posts/reflections/9",
-        "/posts/reflections/10"
+        "/posts/reflections/8",
+        "/posts/reflections/7",
+        "/posts/reflections/6",
+        "/posts/reflections/5",
+        "/posts/reflections/4",
+        "/posts/reflections/3",
+        "/posts/reflections/2",
+        "/posts/reflections/1"
       ],
     },
     {
